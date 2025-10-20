9666616c3eafc9b862a4b5fc3756ee989f6d0e72
Abdeladimka <144343500+Abdeladimka@users.noreply.github.com>
2025-10-20 18:34:02 +0100

    Add function to inverse characters in a string

---
+++ InverseCharachters.js
@@ -0,0 +1,11 @@
+const characterList = "proud to be humain!";
+
+function inverse(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+console.log(inverse(characterList));
