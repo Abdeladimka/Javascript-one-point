603e00d9ecba0ea7aa6146cf29be672bb70f179d
Abdeladimka <144343500+Abdeladimka@users.noreply.github.com>
2025-10-21 08:30:01 +0100

    Add function to count characters in a string
    Implement character counting for a given string.

---
+++ CharNumber.js
@@ -0,0 +1,10 @@
+// Count Characters: Create a function that counts the number of characters in a string.
+const namix = "javascript mastering";
+let CharNumber = 0;
+
+
+for (let i = 0; i < namix.length; i++) {
+  CharNumber++;
+}
+
+console.log(`The number of characters in the string :" ${namix} " is: ${CharNumber}`);
